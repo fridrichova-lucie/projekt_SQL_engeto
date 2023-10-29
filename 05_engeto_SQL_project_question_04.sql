ea5ae4b526ab49f7d2c9d2250e8edc54f9d410a1
user.email <lucie.fridrichova@post.cz>
2023-10-29 21:00:04 +0100

    question_04: finish question

--- 05_engeto_SQL_project_question_04.sql
+++ 05_engeto_SQL_project_question_04.sql
@@ -1,1 +1,31 @@
--- 4.	Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+-- Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+-- Jak je otázka myšlena, byl meziroční nárůst cen potravit větší než 10% - ne žádný rok,
+-- byl nárůst cen větší o více než 10% než nárůst cen mezd - na taky ne.
+
+SELECT DISTINCT 
+	pf.in_year,
+	pf2.in_year,
+	ROUND(AVG(pf2.avg_price_year) / AVG(pf.avg_price_year) *100 -100,2) AS price_percentage,
+	ROUND(AVG(pf2.avg_payroll_year) / AVG(pf.avg_payroll_year) *100 -100,2) AS payroll_percentage,
+	ROUND(AVG(pf2.avg_payroll_year) / AVG(pf.avg_payroll_year) *100 -100,2) - ROUND(AVG(pf2.avg_price_year) / AVG(pf.avg_price_year) *100 -100,2) AS different
+FROM t_lucie_fridrichova_project_sql_primary_final pf 
+JOIN t_lucie_fridrichova_project_sql_primary_final pf2
+	ON pf.in_year = pf2.in_year -1
+	AND pf.category_code = pf2.category_code
+GROUP BY pf.in_year;
+
+-- kompletní příkaz
+SELECT DISTINCT 
+	pf.in_year,
+	pf2.in_year,
+	ROUND(AVG(pf.avg_price_year),2) AS avg_price_all,
+	ROUND(AVG(pf2.avg_price_year),2) AS avg_price_all,
+	ROUND(AVG(pf.avg_payroll_year)) AS avg_payroll_all,
+	ROUND(AVG(pf2.avg_payroll_year)) AS avg_payroll_all,
+	ROUND(AVG(pf2.avg_price_year) / AVG(pf.avg_price_year) *100 -100,2) AS price_percentage,
+	ROUND(AVG(pf2.avg_payroll_year) / AVG(pf.avg_payroll_year) *100 -100,2) AS payroll_percentage
+FROM t_lucie_fridrichova_project_sql_primary_final pf 
+JOIN t_lucie_fridrichova_project_sql_primary_final pf2
+	ON pf.in_year = pf2.in_year -1
+	AND pf.category_code = pf2.category_code
+GROUP BY pf.in_year;
