66c09266089535e43a86cf8c6d57f5ae218de5f1
user.email <lucie.fridrichova@post.cz>
2023-10-31 14:14:44 +0100

    question_05: start with file

---
+++ 06_engeto_SQL_project_question_05.sql
@@ -0,0 +1,2 @@
+-- Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin 
+-- či mzdách ve stejném nebo následujícím roce výraznějším růstem?
