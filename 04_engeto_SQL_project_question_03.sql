0c63af5d5b64cd0d0c4e7cd00256985edbf90daa
user.email <lucie.fridrichova@post.cz>
2023-11-02 15:12:08 +0100

    question_03: small corrections in file

--- 04_engeto_SQL_project_question_03.sql
+++ 04_engeto_SQL_project_question_03.sql
@@ -1,11 +1,11 @@
 -- Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)? 
--- Pokud počítáme i období, když cena klesla, tak cukr a rajčata zlevnily a nejpomaleji cena stoupala
+-- Pokud počítáme i období, kdy cena klesla, tak cukr a rajčata zlevnily a nejpomaleji cena stoupala
 -- u banánů, dále vepřové pečeně a pak minerální vody, jen musím upozornit, že u vína je jen měření posledních 4 let 2015-2018
 
 SELECT DISTINCT 
-	pf.category_code,
-	pf.category_name,
-	round(avg(round(pf2.avg_price_year / pf.avg_price_year *100 -100,2)),2) AS avg_percentage
+	pf.category_code
+	, pf.category_name
+	, round(avg(round(pf2.avg_price_year / pf.avg_price_year *100 -100,2)),2) AS avg_percentage
 FROM t_lucie_fridrichova_project_sql_primary_final pf
 JOIN t_lucie_fridrichova_project_sql_primary_final pf2
 	ON pf.in_year = pf2.in_year -1
