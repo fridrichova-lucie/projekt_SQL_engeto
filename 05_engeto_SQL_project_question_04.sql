d320310146a5360b846179b8e4f31540e190ae94
user.email <lucie.fridrichova@post.cz>
2023-11-02 15:10:18 +0100

    question_04: small corrections in file

--- 05_engeto_SQL_project_question_04.sql
+++ 05_engeto_SQL_project_question_04.sql
@@ -1,13 +1,14 @@
 -- Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
--- Jak je otázka myšlena, byl meziroční nárůst cen potravit větší než 10% - ne žádný rok,
--- byl nárůst cen větší o více než 10% než nárůst cen mezd - na taky ne.
+-- Jak je otázka myšlena, byl meziroční nárůst cen potravin větší než 10% nebo zda byl nárůst cen větší o více než 10% než nárůst cen mezd, 
+-- naštěstí v obou případech nebyl.
 
+-- výsledná tabulka:
 SELECT DISTINCT 
-	pf.in_year,
-	pf2.in_year,
-	ROUND(AVG(pf2.avg_price_year) / AVG(pf.avg_price_year) *100 -100,2) AS price_percentage,
-	ROUND(AVG(pf2.avg_payroll_year) / AVG(pf.avg_payroll_year) *100 -100,2) AS payroll_percentage,
-	ROUND(AVG(pf2.avg_payroll_year) / AVG(pf.avg_payroll_year) *100 -100,2) - ROUND(AVG(pf2.avg_price_year) / AVG(pf.avg_price_year) *100 -100,2) AS different
+	pf.in_year
+	, pf2.in_year AS in_year_next
+	, ROUND(AVG(pf2.avg_price_year) / AVG(pf.avg_price_year) *100 -100,2) AS price_percentage
+	, ROUND(AVG(pf2.avg_payroll_year) / AVG(pf.avg_payroll_year) *100 -100,2) AS payroll_percentage
+	, ROUND(AVG(pf2.avg_payroll_year) / AVG(pf.avg_payroll_year) *100 -100,2) - ROUND(AVG(pf2.avg_price_year) / AVG(pf.avg_price_year) *100 -100,2) AS different
 FROM t_lucie_fridrichova_project_sql_primary_final pf 
 JOIN t_lucie_fridrichova_project_sql_primary_final pf2
 	ON pf.in_year = pf2.in_year -1
@@ -16,14 +17,14 @@ GROUP BY pf.in_year;
 
 -- kompletní příkaz
 SELECT DISTINCT 
-	pf.in_year,
-	pf2.in_year,
-	ROUND(AVG(pf.avg_price_year),2) AS avg_price_all,
-	ROUND(AVG(pf2.avg_price_year),2) AS avg_price_all,
-	ROUND(AVG(pf.avg_payroll_year)) AS avg_payroll_all,
-	ROUND(AVG(pf2.avg_payroll_year)) AS avg_payroll_all,
-	ROUND(AVG(pf2.avg_price_year) / AVG(pf.avg_price_year) *100 -100,2) AS price_percentage,
-	ROUND(AVG(pf2.avg_payroll_year) / AVG(pf.avg_payroll_year) *100 -100,2) AS payroll_percentage
+	pf.in_year
+	, pf2.in_year
+	, ROUND(AVG(pf.avg_price_year),2) AS avg_price_all
+	, ROUND(AVG(pf2.avg_price_year),2) AS avg_price_all
+	, ROUND(AVG(pf.avg_payroll_year)) AS avg_payroll_all
+	, ROUND(AVG(pf2.avg_payroll_year)) AS avg_payroll_all
+	, ROUND(AVG(pf2.avg_price_year) / AVG(pf.avg_price_year) *100 -100,2) AS price_percentage
+	, ROUND(AVG(pf2.avg_payroll_year) / AVG(pf.avg_payroll_year) *100 -100,2) AS payroll_percentage
 FROM t_lucie_fridrichova_project_sql_primary_final pf 
 JOIN t_lucie_fridrichova_project_sql_primary_final pf2
 	ON pf.in_year = pf2.in_year -1
