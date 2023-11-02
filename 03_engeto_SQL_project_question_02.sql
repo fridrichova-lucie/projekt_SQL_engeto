c94c339768c25ba795269ff5187b7801d2f1489e
user.email <lucie.fridrichova@post.cz>
2023-11-02 15:15:06 +0100

    question_02: small corrections in file

--- 03_engeto_SQL_project_question_02.sql
+++ 03_engeto_SQL_project_question_02.sql
@@ -1,7 +1,5 @@
 -- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 -- první období je rok 2006 a poslední je rok 2018 (dalo by se použít i čtvrtletí, ale tabulku mám připravenou na roční hodnoty)
--- mléko má category_code = 114201
--- chleba má category_code = 111301
 -- Nebylo přesně zadáno, zda v jednotlivých odvětvích nebo pouze z průměrné mzdy, zvolila jsem variantu, že jsem počítala množství z průměrné mzdy
 
 -- výsledek:
@@ -9,11 +7,11 @@
 -- chleba bychom si mohli koupit v roce 2006 919 kilo a v roce 2018 už 1051 kilo
 
 SELECT 
-	in_year,
-	category_name,
-	round(avg(avg_payroll_year),0) AS avg_all_payroll,
-	avg_price_year,
-	round(avg_payroll_year / avg_price_year,0) AS how_many
+	in_year
+	, category_name
+	, round(avg(avg_payroll_year),0) AS avg_all_payroll
+	, avg_price_year
+	, round(avg_payroll_year / avg_price_year,0) AS how_many
 FROM t_lucie_fridrichova_project_sql_primary_final tlfpspf
 WHERE in_year IN ('2006', '2018')
 	AND category_code IN ('114201', '111301')
